c2e0693a1c27d768d7a8b568407aa6fec7eac807
AlexandrKoteyko <95771542+AlexandrKoteyko@users.noreply.github.com>
2025-01-19 12:44:12 +0200

    fix

--- maze.py
+++ maze.py
@@ -1,7 +1,7 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
-    def __init__(self,player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image),(65,65))
         self.speed = player_speed
@@ -15,29 +15,31 @@ class GameSprite(sprite.Sprite):
 win_width = 700
 win_height = 500
 class Player(GameSprite):
-    def  update(self):
-        keys=key.get_pressed()
-        if keys[K_LEFT] and self.rect.x>5:
-            self.rect.x -=self.speed
-        if keys[K_RIGHT] and self.rect.x<win_width-80:
-            self.rect.x +=self.speed
-        if keys[K_UP] and self.rect.y>5:
-            self.rect.y -=self.speed
-        if keys[K_DOWN] and self.rect.y<win_height-80:
-            self.rect.y +=self.speed
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
         
 class Enemy(GameSprite):
     direction = "left"
+
     def update(self):
         if self.rect.x <= 470:
             self.direction = "right"
-        if self.rect.x>=win_width-80:
+        if self.rect.x >= win_width - 85:
             self.direction = "left"
 
-        if self.direction=="left":
-            self.rect.x-=self.speed
+        if self.direction == "left":
+            self.rect.x -= self.speed
         else:
-            self.rect.x+=self.speed
+            self.rect.x += self.speed
 
 
 
@@ -45,17 +47,18 @@ window=display.set_mode((win_width, win_height))
 display.set_caption("Maze")
 background = transform.scale(image.load("background.jpg"),(win_width, win_height))
 
-player = GameSprite("hero.png",5, win_height - 80, 4)
-monster = GameSprite("cyborg.png", win_width - 80, 280, 2)
-final= GameSprite("treasure.png", win_width - 100, win_height - 80, 0)
+player = Player('hero.png', 5, win_height - 80, 4)
+monster = Enemy('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+
 
 game = True
-clock=time.Clock()
+finish = False
+clock = time.Clock()
 FPS = 60
-finish=False
 
 mixer.init()
-mixer.musics.load("jungles.ogg")
+mixer.music.load("jungles.ogg")
 mixer.music.play()
 
 
@@ -64,12 +67,11 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    if finish!=True:
 
-        window.blit(background,(0,0))
+    if finish != True:
+        window.blit(background, (0, 0))
         player.update()
         monster.update()
-
 
         player.reset()
         monster.reset()
